1b70e005d5b306956d05b78ffe6e36d71ccfc9a5
Ivan G <mr.zero@inbox.ru>
2023-10-28 20:17:26 +0300

    feat: implement search

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,7 +3,6 @@ import NewsApiResponse from '../../interface/newsapiresponse';
 import NewsApiArticle from '../../interface/newsapiarticle';
 import NewsSnippet from '../NewsSnippet/NewsSnippet';
 
-// const apiURL = 'https://newsapi.org/v2/everything';
 const apiURL = 'https://newsapi.org/v2/top-headlines';
 // const apiURL = 'http://127.0.0.1:8075/everything';
 
@@ -16,44 +15,47 @@ type ContentProps = {
 type ContentState = {
   loading: boolean;
   content: null | NewsApiResponse;
+  url: string;
+  query?: string;
 };
 
-interface Options {
+type Options = {
   pageSize: number;
   page: number;
-}
-
-const options: Options = {
-  pageSize: 10,
-  page: 1,
 };
 
-const makeURL = () => {
-  if (!apiURL || !apiKey) {
-    throw new Error('Invalid request');
-  }
-  let url = apiURL;
-  if (options) {
-    url = url + '?';
-    for (const key in options) {
-      url = url + `${key}=${options[key as keyof Options]}&`;
-    }
-    url = url.slice(0, -1);
-  }
-  return `${url}&language=en&apiKey=${apiKey}`;
-};
-
-async function apiCall(): Promise<NewsApiResponse> {
-  const res = await fetch(makeURL());
-  const data = await res.json();
-  return data;
-}
+// -----------------------------------------------------------------------------------------------------
 
 class Content extends Component<ContentProps> {
   state: ContentState = {
     loading: false,
     content: null,
+    url: '',
+    query: this.props.query,
   };
+
+  makeURL(query?: string, options: Options = { pageSize: 10, page: 1 }) {
+    if (!apiURL || !apiKey) {
+      throw new Error('Invalid request');
+    }
+    let url = apiURL + '?';
+    for (const key in options) {
+      url = url + `${key}=${options[key as keyof Options]}&`;
+    }
+    url = url.slice(0, -1);
+    if (query) {
+      url = url + `&q=${encodeURIComponent(query)}`;
+    }
+    url = url + `&language=en&apiKey=${apiKey}`;
+    this.setState({ url: url });
+    return url;
+  }
+
+  async apiCall(query?: string): Promise<NewsApiResponse> {
+    const res = await fetch(this.makeURL(query));
+    const data = await res.json();
+    return data;
+  }
 
   renderContent() {
     const data = this.state.content as NewsApiResponse;
@@ -74,7 +76,7 @@ class Content extends Component<ContentProps> {
     this.setState({
       loading: true,
     });
-    apiCall().then((res) => {
+    this.apiCall(this.props.query).then((res) => {
       this.setState({
         content: res,
       });
@@ -84,6 +86,24 @@ class Content extends Component<ContentProps> {
     });
   }
 
+  componentDidUpdate(prevProps: ContentProps) {
+    if (this.props.query !== prevProps.query) {
+      this.setState({
+        loading: true,
+      });
+      this.apiCall(this.props.query).then((res) => {
+        this.setState({
+          content: res,
+        });
+        this.setState({
+          loading: false,
+        });
+      });
+    }
+  }
+
+  componentWillUnmount() {}
+
   render() {
     return (
       <div>
@@ -92,8 +112,8 @@ class Content extends Component<ContentProps> {
           <h2>Loading...</h2>
         ) : (
           <div>
-            <h3>{this.props.query}</h3>
-            <div>{makeURL()}</div>
+            {this.props.query && <h3>Search for: {this.props.query}</h3>}
+            <div>{this.state.url}</div>
             {this.renderContent()}
           </div>
         )}
