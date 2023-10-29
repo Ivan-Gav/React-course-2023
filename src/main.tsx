9bbd964011a07bcf8a9b668d28302e0d8367b119
Ivan G <mr.zero@inbox.ru>
2023-10-29 17:13:32 +0300

    feat: add fallback component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import Fallback from './components/Fallback/Fallback.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary fallback={<h1>This is your ERROR bro!</h1>}>
+    <ErrorBoundary fallback={<Fallback />}>
       <App />
     </ErrorBoundary>
   </React.StrictMode>
