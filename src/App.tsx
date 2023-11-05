9b74d58dea1a73de88375001013c35cda3b141e5
Ivan G <mr.zero@inbox.ru>
2023-11-05 23:11:13 +0300

    feat: set up error element

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import NewsDetails from './components/NewsDetails/NewsDetails';
+import Fallback from './components/Fallback/Fallback';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+    errorElement: <Fallback />,
     children: [
       {
         path: ':article',
--- src/components/Fallback/Fallback.tsx
+++ src/components/Fallback/Fallback.tsx
@@ -1,11 +1,15 @@
+import { useNavigate } from 'react-router';
+
 function Fallback() {
+  const navigate = useNavigate();
+
   return (
-    <>
+    <div className="fallback">
       <h1>This is your ERROR bro!</h1>
-      <button type="button" onClick={() => window.location.reload()}>
-        Reload page
+      <button type="button" onClick={() => navigate('/')}>
+        Go to Main page
       </button>
-    </>
+    </div>
   );
 }
 
